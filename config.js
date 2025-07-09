9232b0739787acc384c9be24d6840fbc6768b76b
eksuhomestv <eksuhomestv@gmail.com>
2025-07-09 14:18:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💚,❤️,🤍",
 // chose custom react emojis by yourself 
